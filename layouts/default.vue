e142f3b072c984ee8b36b620b5307c81fea47530
Metegultekin98 <metegultekin98@hotmail.com>
2022-07-22 07:18:19 +0300

    Initial

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <AppHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from '~/components/AppHeader'
+
+export default {
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  line-height: 1.6;
+  background: #f4f4f4;
+}
+
+a {
+  color: #666;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+.container {
+  max-width: 800px;
+  margin: 2rem auto;
+  overflow: hidden;
+  padding: 1rem 2rem;
+  background: #fff;
+}
+</style>
